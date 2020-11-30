0e0d704e75d9b8acb24dae34f09f11cfe3e9a5b2
Tanner Strickler <tanner.strickler@goldkey.com>
2020-11-30 16:31:45 -0600

    Add project files.

---
+++ F9PBugDemo/F9PBugDemo/EntryWithPopup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BubblePopup = Forms9Patch.BubblePopup;
+
+namespace F9PBugDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryWithPopup : ContentView
+    {
+        BubblePopup popup;
+        public EntryWithPopup()
+        {
+            InitializeComponent();
+            var content = new Frame
+            {
+                Padding = 0,
+                Margin = 0,
+                Content = new Label { Text = "Popup!" }
+            };
+            popup = new BubblePopup(MainEntry)
+            {
+                PointerLength = 0,
+                TargetBias = 0,
+                PageOverlayColor = Color.Transparent,
+                Content = content,
+                CloseWhenBackgroundIsClicked = false,
+                BackgroundInputTransparent = false // this doesn't work regardless of whether true or false
+            };
+        }
+
+        void OnTextChanged(object sender, TextChangedEventArgs args)
+        {
+            var entry = sender as Entry;
+            popup.IsVisible = entry.Text.Length > 0;
+        }
+    }
+}
---
+++ F9PBugDemo/F9PBugDemo/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace F9PBugDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
